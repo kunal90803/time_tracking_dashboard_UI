84451a7aa75147c2f7eb7c1a8e6558ab9147f2fe
kunal90803 <palk27530@gmail.com>
2025-02-09 00:48:39 +0530

    ui_update

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-
-    
+async function dataCollector(){
+        const url="./data.json";
+        try {
+                const response=await fetch(url);
+                if(!response.ok){
+                        throw new Error(`response status: ${response.status}`);
+                }
+                const json=await response.json();
+                console.log(json[3]);
+        } catch (error) {
+                console.log(error.message);
+        }
+}
+dataCollector();
